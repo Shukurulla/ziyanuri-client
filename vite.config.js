465af982d7c3d18f9882f3ffdd56532a78f6a522
Shukurulla <shukurullatursinbayev978@gmail.com>
2026-03-21 12:37:12 +0500

    add host 0.0.0.0

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: '0.0.0.0',
     proxy: {
       "/api": "http://localhost:7294",
       "/uploads": "http://localhost:7294",
