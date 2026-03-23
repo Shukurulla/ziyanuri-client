5aeba86aef5edcb903ff1bcba14f212cc0464d6e
Shukurulla <shukurullatursinbayev978@gmail.com>
2026-03-23 05:07:20 +0500

    start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "ziyanuri-client",
     script: "npx",
-    args: "serve dist -l 3000 -s",
+    args: "serve dist -l 4173 -s",
     cwd: "/root/shukurulla/ziyanuri-client",
     env: {
       PM2_SERVE_SPA: "true",
